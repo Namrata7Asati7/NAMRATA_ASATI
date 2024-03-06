d3c72881f1940718ae851c61f88f2e0545c9640e
Namrata7Asati7 <namrataasati3@gmail.com>
2024-03-07 00:18:52 +0530

    New Prpject 3

--- script.js
+++ script.js
@@ -71,3 +71,29 @@ $(document).ready(function(){
         }
     });
 });
+
+// For About Section
+
+
+var tablinks = document.getElementsByClassName("tab-links");
+var tabcontents = document.getElementsByClassName("tab-contents");
+
+function opentab(tabname) {
+    for(tablink of tablinks) {
+    tablink.classList.remove("active-link");
+}
+for(tabcontent of tabcontents) {
+ tabcontent.classList.remove("active-tab");
+ }
+ event.currentTarget.classList.add("active-link");
+ document.getElementById(tabname).classList.add("active-tab");
+}
+
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
